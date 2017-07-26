6d982447ca771a54fb1a8c56ffdb680cb95ce2f4
saivanov93 <s.a.ivanov93@gmail.com>
2017-07-26 14:50:10 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mindbox
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        static public double GetSquare(double a, double b, double c)
+        {
+            double aa = Math.Round(a * a, 5);
+            double bb = Math.Round(b * b, 5);
+            double cc = Math.Round(c * c, 5);
+
+            if (cc == (aa + bb)) return a * b / 2;
+            if (aa == (cc + bb)) return c * b / 2;
+            if (bb == (aa + cc)) return a * c / 2;
+
+            return -1;
+        }
+        static public double GetSquare(int a, int b, int c)
+        {
+
+            return GetSquare((double)a, (double)b, (double)c);
+        }
+
+        
+        static public double GetSquare(float a, float b, float c)
+        {
+            return GetSquare((double) a, (double) b, (double) c);
+        }
+
+
+    }
+}
---
+++ ProgramTests.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mindbox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mindbox.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void GetSquareMinusOne()
+        {
+            //arrange
+            var a = 0;
+            var b = 1;
+            var c = 2;
+            //act
+            double result = Program.GetSquare(a,b,c);
+            double expected = -1;
+            //assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void GetSquareSixDouble()
+        {
+            //arrange
+            var a = 3.0;
+            var b = 4.0;
+            var c = 5.0;
+            //act
+            double result = Program.GetSquare(a, b, c);
+            double expected = 6;
+            //assert
+            Assert.AreEqual(expected, result); 
+        }
+
+        [TestMethod()]
+        public void GetSquareSixFloat()
+        {
+            //arrange
+            var a = 3.0f;
+            var b = 4.0f;
+            var c = 5.0f;
+            //act
+            double result = Program.GetSquare(a, b, c);
+            double expected = 6;
+            //assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void GetSquareSixInt()
+        {
+            //arrange
+            var a = 3;
+            var b = 4;
+            var c = 5;
+            //act
+            double result = Program.GetSquare(a, b, c);
+            double expected = 6;
+            //assert
+            Assert.AreEqual(expected, result);
+        }
+
+    }
+}
